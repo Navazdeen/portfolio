68df31149414ca58d98350ec3953e20b1f489412
Navazdeen <navazdeen1999@gmail.com>
2024-02-23 00:21:53 +0530

    add project cards with github

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -1,12 +1,9 @@
 import Hero from '@/components/hero'
-import AreaInterest from '@/components/interest'
-import Intro from '@/components/intro'
-import Skills from '@/components/skills'
 
 export default function Home() {
   return (
-      <div className=''>
-        <Hero></Hero>
-      </div>
+    <div className=''>
+      <Hero></Hero>
+    </div>
   )
 }
--- src/app/(routes)/projects/page.tsx
+++ src/app/(routes)/projects/page.tsx
@@ -1,9 +1,47 @@
+'use client'
+import ProjectCard from "@/components/projectCard"
+import { useEffect, useState } from "react";
 
+interface Repo {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export default function Projects() {
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch('https://api.github.com/user/repos', {
+          headers: {
+            Authorization: `Bearer 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`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch GitHub repos');
+        }
+
+        const data = await response.json();
+        setRepos(data);
+      } catch (error) {
+        console.error('Error fetching GitHub repos:', error);
+      }
+    };
+
+    fetchRepos();
+  }, []);
   return (
-      <div className='flex h-2/3 bg-orange-400'>
-        Projects
+    <div className='flex py-12 px-4 flex-col'>
+      <div className="font-bold text-4xl py-8">Some of my projects</div>
+      <div className="grid grid-cols-3 gap-y-8 pb-8 px-4">
+        {repos.map((repo) => (
+          <ProjectCard key={repo.id} heading={repo.name} description={repo.description} link={repo.html_url} />
+        ))}
       </div>
+    </div>
   )
 }
