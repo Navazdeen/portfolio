9bb8368c5422b93b615ada3ce11214b684b81428
Navazdeen <navazdeen1999@gmail.com>
2024-03-03 17:02:03 +0530

    add public env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GITHUB_TOKEN:
+      "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",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/(routes)/projects/page.tsx
+++ src/app/(routes)/projects/page.tsx
@@ -1,23 +1,25 @@
 'use client'
 import ProjectCard from "@/components/projectCard"
+import { ChevronsLeftIcon, ChevronsRightLeft } from "lucide-react";
 import { useEffect, useState } from "react";
 
 interface Repo {
   id: number;
   name: string;
-  description: string;
+  description: string;  
   html_url: string;
 }
 
 export default function Projects() {
   const [repos, setRepos] = useState<Repo[]>([]);
 
+  console.log(process.env.NEXT_PUBLIC_GITHUB_TOKEN)
   useEffect(() => {
     const fetchRepos = async () => {
       try {
         const response = await fetch('https://api.github.com/user/repos', {
           headers: {
-            Authorization: `Bearer ${process.env.pat_token}`,
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
           },
         });
 
