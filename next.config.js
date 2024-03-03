fa47a2804f2421cf1d7104c53133475de281fb97
Navazdeen <navazdeen1999@gmail.com>
2024-03-03 17:16:50 +0530

    remove env

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_GITHUB_TOKEN:
-      "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",
-  },
-};
-
-module.exports = nextConfig;
